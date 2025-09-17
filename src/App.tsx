6a54174674634b22c32cb7cf9240fa8d8dea6908
evaxliu <evaxinerliu@gmail.com>
2025-09-16 21:01:43 -0700

    animation for about

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,33 @@ import './styles/App.css';
 import React, { useState } from 'react';
 import { LofiRoom } from './components/LofiRoom';
 import About from './pages/About';
+import Projects from './pages/Projects';
+import Experience from './pages/Experience';
+import { motion } from 'framer-motion';
 // import { ProjectBox } from './components/ProjectBox';
 // import HomePage from './pages/HomePage';
 // import AboutPage from './pages/AboutPage';
 // import ContactPage from './pages/ContactPage';
 
 function App() {
-  const [ showHide, setShowHide ] = useState(false);
+  const [ component, setComponent ] = useState('');
 
-  const handleToggle = () => {
-    setShowHide(!showHide)
+  const handleToggleHome = () => {
+    setComponent('')
   }
+
+  const handleToggleAbout = () => {
+    setComponent('about')
+  }
+
+  const handleToggleProjects = () => {
+    setComponent('project')
+  }
+
+  const handleToggleExperience = () => {
+    setComponent('experience')
+  }
+  
 
   return (
     <div className="App">
@@ -22,13 +38,16 @@ function App() {
         </p>
         <main>
           <nav>
-            <button onClick={handleToggle}>
+            <button onClick={handleToggleHome}>
+              Home
+            </button>
+            <button onClick={handleToggleAbout}>
               About
             </button>
-            <button>
+            <button onClick={handleToggleExperience}>
               Experience
             </button>
-            <button>
+            <button onClick={handleToggleProjects}>
               Projects
             </button>
           </nav>
@@ -36,11 +55,21 @@ function App() {
             <div id='lofi-room'>
               <LofiRoom />
             </div>
-            {showHide && (<About />)}
+            {component === "about" && (
+              <motion.div
+                key="about"
+                className="about-motion-wrapper"
+                initial={{ opacity: 0, x: 50 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: -50 }}
+                transition={{ duration: 0.4 }}
+              >
+                <About />
+              </motion.div>
+            )}
+            {component === 'project' && (<Projects />)}
+            {component === 'experience' && (<Experience />)}
           </div>
-          {/* <div id='project'>
-            <ProjectBox />
-          </div> */}
         </main>
       </header>
     </div>
