7d260853f3aa767d26f46196f8ef820c67f67e1d
evaxliu <evaxinerliu@gmail.com>
2025-12-13 17:02:03 -0800

    remove unused react router

--- src/app/components/ProjectBox.tsx
+++ src/app/components/ProjectBox.tsx
@@ -1,6 +1,5 @@
 import '../styles/ProjectBox.css'
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export const ProjectBox = () => {
   return (
