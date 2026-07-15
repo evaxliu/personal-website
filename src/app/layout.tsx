9f0771e91dc359c05a5bd5f07cb669b61651034e
evaxliu <evaxinerliu@gmail.com>
2026-07-14 20:09:04 -0700

    change open graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
     siteName: "Eva Liu",
     images: [
       {
-        url: "/open-graph.png",
+        url: "/open-graph-image.png",
         width: 1200,
         height: 630,
         alt: "Eva Liu: Software Engineer and Freelance Web Developer",
