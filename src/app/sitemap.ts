2aa6af21f6db4b4e60b98473c6abd8df720deaaa
Eva L <69877857+evaxliu@users.noreply.github.com>
2026-07-23 04:57:52 -0700

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const BASE_URL = "https://www.lilacplanet.dev";
+const BASE_URL = "https://evaxliu.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
